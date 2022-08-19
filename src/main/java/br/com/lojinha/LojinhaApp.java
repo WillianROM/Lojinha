49b0e3cd9d7c520ac4d7ed0544229eb96b934cab
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-08-19 18:39:45 -0300

    Aula PTQS - Definindo como as coisas serão
    Definindo como as coisas serão

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,12 +1,28 @@
 package br.com.lojinha;
 
+import br.com.lojinha.pojo.Produto;
+
 public class LojinhaApp {
     public static void main(String[] args) { //Método principal é criado depois de digitar psvm
+        /* Aula de Variáveis
         String joias = "100gr de Ouro 50k";
         int minhaIdade = 18;
         double meuSalario = 2022.51;
         boolean minhaResposta = true;
 
         System.out.println(minhaIdade);
+         */
+
+        Produto meuProduto = new Produto();
+
+        meuProduto.nome = "Caneta";
+        meuProduto.valor = 1.25;
+        meuProduto.marca = "Bic";
+        meuProduto.tamanho = "Medio";
+        meuProduto.itensInclusos = "tampa";
+
+        System.out.println(meuProduto.nome);
+
+
     }
 }
---
+++ src/main/java/br/com/lojinha/pojo/Produto.java
@@ -0,0 +1,10 @@
+package br.com.lojinha.pojo;
+
+public class Produto {
+    //Atributos
+    public String nome;
+    public String marca;
+    public double valor;
+    public String tamanho;
+    public String itensInclusos;
+}
