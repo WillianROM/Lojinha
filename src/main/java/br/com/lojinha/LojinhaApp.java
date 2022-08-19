00b187cbe92bf286128ec19223492a4b9d7c8d90
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-08-18 23:49:25 -0300

    Aula de Variáveis do PTQS

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,7 +1,12 @@
 package br.com.lojinha;
 
 public class LojinhaApp {
-    public static void main(String[] args) { //Método principal depois de digitar psvm
-        System.out.println("Olá, mundo!");
+    public static void main(String[] args) { //Método principal é criado depois de digitar psvm
+        String joias = "100gr de Ouro 50k";
+        int minhaIdade = 18;
+        double meuSalario = 2022.51;
+        boolean minhaResposta = true;
+
+        System.out.println(minhaIdade);
     }
 }
